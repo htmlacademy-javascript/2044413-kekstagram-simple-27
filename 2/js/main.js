7a27f43389d771368f1f76364ce3eac477fd45b8
Keks <keks@htmlacademy.ru>
2022-10-11 20:56:55 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,9 +1,13 @@
-// ДЗ Module2-task1-function1
-const getRandomInteger = function (fromValue, toValue) {
+// ФУНКЦИЯ,КОТОРАЯ ВОЗВРАЩАЕТ СЛУЧАЙНОЕ ЦЕЛОЕ ЧИСЛО ИЗ ПЕРЕДАННОГО ДИАПАЗОНА ВКЛЮЧИТЕЛЬНО.
+// РЕЗУЛЬТАТ:ЦЕЛОЕ ЧИСЛО ИЗ ДИАПАЗОНА "ОТ...ДО".
+// АРГУМЕНТАМИ ФУНКЦИИ МОГУТ БЫТЬ ТОЛЬКО ПОЛОЖИТЕЛЬНЫЕ ЧИСЛА И НОЛЬ.ЕСЛИ ФУНКЦИИ ПРИШЛИ НЕПРАВИЛЬНЫЕ АРГУМЕНТЫ,ОНА ДОЛЖНА ВЕРНУТЬ NaN.
+// ДЗ Module2-task1-function1.
+function getRandomInteger(fromValue, toValue) {
   if (fromValue < 0 || toValue < 0) {
     return NaN;
   }
-
+  // ПРИДУМАЛА,КАК ФУНКЦИЯ БУДЕТ ВЕСТИ СЕБЯ,ЕСЛИ ПЕРЕДАТЬ ЗНАЧЕНИЕ "ДО" МЕНЬШЕ,ЧЕМ ЗНАЧЕНИЕ "ОТ",ИЛИ РАВНОЕ ЕМУ.
+  // ФУНКЦИЯ МОЖЕТ ВЕРНУТЬ ЗНАЧЕНИЕ NaN.
   if (toValue <= fromValue) {
     return NaN;
   }
@@ -11,20 +15,28 @@ const getRandomInteger = function (fromValue, toValue) {
   const randomValue = fromValue + Math.random() * (toValue - fromValue);
 
   return Math.round(randomValue);
-};
+}
 
 // eslint-disable-next-line no-console
 console.log(getRandomInteger(0, 1000));
 
 
-// ДЗ Module2-task1-function2
-const checkMaxLineLenght = function (checkingString, maxLenght) {
-  if (checkingString.length > maxLenght) {
-    return false;
-  } else {
-    return true;
-  }
-};
+// ДЗ Module2-task1-function2.
+// ФУНКЦИЯ ДЛЯ ПРОВЕРКИ МАКСИМАЛЬНОЙ ДЛИНЫ СТРОКИ.БУДЕТ ИСПОЛЬЗОВАТЬСЯ ДЛЯ ПРОВЕРКИ ДЛИНЫ ВВЕДЕННОГО КОММЕНТАРИЯ,НО ДОЛЖНА БЫТЬ УНИВЕРСАЛЬНА.
+// НАПИСАН МОЙ ВАРИАНТ ЭТОЙ ФУНКЦИИ.
+// const checkMaxLineLenght = function (checkingString, maxLenght) {
+//   if (checkingString.length > maxLenght) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// };
 
-// eslint-disable-next-line no-console
+// НАПИСАН ВАРИАНТ ЭТОЙ ЖЕ ФУНКЦИИ ПРЕПОДАВАТЕЛЕМ.(ЭТОТ ВАРИАНТ НАПИСАНИЯ ПРАВИЛЬНЫЙ)
+// eslint-disable-next-line no-unused-vars
+function checkMaxLineLength(checkingString, maxLength) {
+  return checkingString.length < maxLength;
+}
+
+// eslint-disable-next-line no-console, no-undef
 console.log(checkMaxLineLenght('Hello', 500));
