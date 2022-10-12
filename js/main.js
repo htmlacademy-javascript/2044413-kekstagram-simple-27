29386aadec3ccc07a07f51207d0d0206512ec040
Khodas Darya <daria106226@mail.ru>
2022-10-13 00:03:18 +0300

    Домашняя работа по Генерации данных. Больше деталей по ДЗ 4

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ console.log(getRandomInteger(0, 1000));
 // ДЗ Module2-task1-function2.
 // ФУНКЦИЯ ДЛЯ ПРОВЕРКИ МАКСИМАЛЬНОЙ ДЛИНЫ СТРОКИ.БУДЕТ ИСПОЛЬЗОВАТЬСЯ ДЛЯ ПРОВЕРКИ ДЛИНЫ ВВЕДЕННОГО КОММЕНТАРИЯ,НО ДОЛЖНА БЫТЬ УНИВЕРСАЛЬНА.
 // НАПИСАН МОЙ ВАРИАНТ ЭТОЙ ФУНКЦИИ.
-// const checkMaxLineLenght = function (checkingString, maxLenght) {
+// const checkMaxLineLength = function (checkingString, maxLenght) {
 //   if (checkingString.length > maxLenght) {
 //     return false;
 //   } else {
@@ -45,4 +45,46 @@ function checkMaxLineLength(checkingString, maxLength) {
 }
 
 // eslint-disable-next-line no-console, no-undef
-console.log(checkMaxLineLenght('Hello', 500));
+console.log(checkMaxLineLength('Hello', 500));
+
+// ДЗ Module4-task1 //
+// создаем массив с описанием фотографий
+const PHOTO_DESCRIPTION_LIST = [
+  'Отель на берегу',
+  'Дорога на пляж',
+  'Пляж на берегу океана',
+  'Рисовые человечки',
+  'Авто будущего',
+  'Вкусный завтрак',
+  'Ягодный напиток',
+];
+
+// создаем функцию, которая будет генерировать(создавать) объект
+const generatePhotoData = function (value) {
+  const data = {
+    id: value,
+    url: `photos/${value}.jpg`,
+    description: PHOTO_DESCRIPTION_LIST[getRandomInteger(0, PHOTO_DESCRIPTION_LIST.length - 1)],
+    likes: getRandomInteger(15, 200),
+    comments: getRandomInteger(0, 200),
+  };
+
+  return data;
+};
+
+// создаем функцию, которая будет генерировать(создавать) 25 объектов(список объектов)
+const generatePhotoDataList = function () {
+  const list = [];
+
+  for (let i = 1; i <= 25; i++) {
+    const photoData = generatePhotoData(i);
+    list.push(photoData);
+  }
+
+  return list;
+};
+
+const photoDataList = generatePhotoDataList();
+// eslint-disable-next-line no-console
+console.log(photoDataList);
+
