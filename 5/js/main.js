31347b7fd6d45a5dafac138cb03b56ef20caed14
Keks <keks@htmlacademy.ru>
2022-10-26 15:01:06 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,12 @@
+// main.js - это точка входа. Модуль, который связывает другие модули
+// Задача точки входа — подключить и настроить всё, что нужно для работы страницы или даже целого приложения
+import { checkMaxLineLength } from './util.js';
+import { generatePhotoDataList } from './data.js';
+import './picturesRendering.js';
+
+// eslint-disable-next-line no-console
+console.log(checkMaxLineLength('Hello, JavaScript!', 10));
+
+const photoDataList = generatePhotoDataList();
+// eslint-disable-next-line no-console
+console.log(photoDataList);
