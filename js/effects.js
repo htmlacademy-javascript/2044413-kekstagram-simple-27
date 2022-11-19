62dd3152caad2f182f2ccd881573c760d9b858e0
Khodas Darya <daria106226@mail.ru>
2022-11-20 01:10:40 +0300

    ДОБАВИЛА СЛАЙДЕР ДЛЯ ИЗМЕНЕНИЯ ГЛУБИНЫ ЭФФЕКТА

--- js/effects.js
+++ js/effects.js
@@ -6,6 +6,12 @@ const previewElement = document.querySelector('.img-upload__preview img');
 // найдем список эффектов
 const effectsListElement = document.querySelector('.effects__list');
 
+// найдем слайдер
+const sliderElement = document.querySelector('.effect-level__slider');
+
+// найдем элемент для хранения значения слайдера
+const valueSliderElement = document.querySelector('.effect-level__value');
+
 // создадим объект эффектов
 const effects = {
   none: {
@@ -15,29 +21,113 @@ const effects = {
   chrome: {
     name: 'chrome',
     filter: 'grayscale',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    step: 0.1,
+    measurement: '',
   },
   sepia: {
     name: 'sepia',
     filter: 'sepia',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    step: 0.1,
+    measurement: '',
   },
   marvin: {
     name: 'marvin',
     filter: 'invert',
+    range: {
+      min: 0,
+      max: 100,
+    },
+    step: 1,
+    measurement: '%',
   },
   phobos: {
     name: 'phobos',
-    filter: 'blue',
+    filter: 'blur',
+    range: {
+      min: 0,
+      max: 3,
+    },
+    step: 0.1,
+    measurement: 'px',
   },
   heat: {
     name: 'heat',
     filter: 'brightness',
+    range: {
+      min: 1,
+      max: 3,
+    },
+    step: 0.1,
+    measurement: '',
   },
 };
 
+// текущие настройки эффекта
+let currentEffect = effects.none;
+
 // функция, которая добавляет эффект изображению
 const addEffect = function (effect) {
+  previewElement.style.filter = null;
   previewElement.className = '';
   previewElement.classList.add(`effects__preview--${effect.name}`);
+
+  const previousEffect = currentEffect;
+  currentEffect = effect;
+
+  if (effect.name === 'none') {
+    valueSliderElement.value = '';
+
+    if (sliderElement.noUiSlider) {
+      sliderElement.noUiSlider.destroy();
+    }
+  } else {
+    valueSliderElement.value = effect.range.max;
+
+    if (previousEffect.name === 'none') {
+      noUiSlider.create(sliderElement, {
+        range: {
+          min: effect.range.min,
+          max: effect.range.max,
+        },
+        start: effect.range.max,
+        step: effect.step,
+        connect: 'lower',
+        format: {
+          to: function (value) {
+            if (Number.isInteger(value)) {
+              return value.toFixed(0);
+            }
+            return value.toFixed(1);
+          },
+          from: function (value) {
+            return parseFloat(value);
+          },
+        }
+      });
+
+      sliderElement.noUiSlider.on('update', () => {
+        valueSliderElement.value = sliderElement.noUiSlider.get();
+        previewElement.style.filter = `${currentEffect.filter}(${valueSliderElement.value}${currentEffect.measurement})`;
+      });
+    } else {
+      sliderElement.noUiSlider.updateOptions({
+        range: {
+          min: effect.range.min,
+          max: effect.range.max,
+        },
+        start: effect.range.max,
+        step: effect.step,
+      });
+    }
+  }
 };
 
 // функция, для обработчика события кликов на эффекты
@@ -52,6 +142,12 @@ const onAddEffect = function (evt) {
 // функция, которая сбрасывает эффект изображению
 const resetEffect = function () {
   previewElement.className = '';
+  previewElement.style.filter = null;
+  currentEffect = effects.none;
+
+  if (sliderElement.noUiSlider) {
+    sliderElement.noUiSlider.destroy();
+  }
 };
 
 // добавим обработчик события по кликам на эффекты
